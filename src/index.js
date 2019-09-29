3d705efa67a3fa8d272c3d88edd4d9266b6fff0b
sharkpp <touroku@sharkpp.net>
2019-09-29 20:47:19 +0900

    変更 コメントを日本語化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+"use struct";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// アプリをオフラインで動作させてより速くロードしたい場合は、以下で unregister() を
+// register() に変更できます。これにはいくつかの落とし穴があります。
+// サービスワーカーの詳細: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
+// 要求スペック
+//   config#onUpdate(registration);
+//   config#onSuccess(registration);
+// serviceWorker.register(config);
