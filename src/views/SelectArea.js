e4e2319eddabc7c0077648dfcf5e5b58c10bc3ee
sharkpp <sharkpp@users.noreply.github.com>
2019-10-01 20:03:43 +0900

    修正 範囲選択

--- src/views/SelectArea.js
+++ src/views/SelectArea.js
@@ -9,75 +9,80 @@ export default function ({ onNextStep, image }) {
 
   const [ canvasRef, setCanvasRef ] = useState(createRef());
   const [ canvasMaskRef, setCanvasMaskRef ] = useState(createRef());
+  const [ canvasOffscreenRef, setCanvasOffscreenRef ] = useState(createRef());
   const [crop, setCrop] = useState({ });
 
   useEffect(() => {
 
+    // 画像を描画
     const canvas = canvasRef.current;
     canvas.width = image.width;
     canvas.height= image.height;
     const ctx    = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    const canvasOffscreen = canvasOffscreenRef.current;
+    canvasOffscreen.width = image.width;
+    canvasOffscreen.height= image.height;
+
   }, [image]);
 
-  const handleAreaLock = useCallback(() => {
+  // 範囲変更
+  const handleChangeArea = useCallback((newCrop) => {
     const canvas = canvasRef.current;
     const ctx    = canvas.getContext('2d');
-    const scale = image.width  / canvas.clientWidth;
-    //const scaleY = image.height / canvas.clientHeight;
+    const scale  = image.width / canvas.clientWidth;
+
+    const canvasOffscreen = canvasOffscreenRef.current;
+    const ctxOffscreen    = canvasOffscreen.getContext('2d');
+    ctxOffscreen.drawImage(image, 0, 0, image.width, image.height);
 
     const canvasMask = canvasMaskRef.current;
-    canvasMask.width = crop.width;
-    canvasMask.height= crop.height;
+    canvasMask.width = crop.width  / 5;
+    canvasMask.height= crop.height / 5;
     const ctxMask    = canvasMask.getContext('2d');
+
+    if (canvasMask.width < 1 || canvasMask.height < 1) {
+      return;
+    }
+
     ctxMask.fillStyle = '#000000';
     ctxMask.fillRect(0, 0, canvasMask.width, canvasMask.height);
-    ctxMask.fillStyle = '#000000';
+    ctxMask.fillStyle = '#FF0000';
+    ctxMask.beginPath();
+    ctxMask.ellipse(canvasMask.width / 2, canvasMask.height / 2, canvasMask.width / 2 * 0.9, canvasMask.height / 2 * 0.9, 0, 0, 2 * Math.PI);
+    ctxMask.fill();
+    const maskData = ctxMask.getImageData(0, 0, canvasMask.width, canvasMask.height).data; // [ R(0,0), G(0,0), B(0,0), alpha(0,0), ... ]
 
-    ctx.fillStyle = 'rgb(0,0,0,0.5)';
-    ctx.fillRect(crop.x * scale, crop.y * scale, crop.width * scale, crop.height * scale);
-
-
-
-    /*
+    const xyStep  = 5 * scale;
     const xOffset = crop.x * scale;
-    const xMax = crop.width * scale;
-    const xStep= 5 * scale;
-    const xMid = xMax / 2;
+    const xMax    = xOffset + crop.width * scale;
     const yOffset = crop.y * scale;
-    const yMax = crop.height * scale;
-    const yStep= 5 * scale;
-    const yMid = yMax / 2;
-    ctx.fillStyle = '#FFFFFF';
-    const cMax = yMax * xMax;
-    for (let y = 0.0, yi = 0; y < yMax; y += yStep, ++yi) {
-      for (let x = 0.0, xi = 0; x < xMax; x += xStep, ++xi) {
-        if (!(y<yMid/2&&x<xMid/2))continue;
-        if (1||Math.random() < 0.4) {
-          const deg = Math.atan2(y - yMid, x - xMid);
-          const xCircle = Math.cos(deg) * xMid;
-          const yCircle = Math.sin(deg) * yMid;
-          const dCircle = Math.sqrt(Math.abs(xCircle * xCircle) + Math.abs(yCircle * xCircle));
-          const dxy = Math.sqrt(Math.abs((x - xMid) * (x - xMid)) + Math.abs((y - yMid) * (y - yMid)));
-          //console.log([xi,yi,dCircle,dxy,dxy < dCircle]);
-          //const xf = xCircle < xMid ? x < xCircle : xCircle < x;
-          //const yf = yCircle < yMid ? y < yCircle : yCircle < y;
-          console.log([xi,yi,x/xStep,y/xStep,deg,(xCircle+xMid)/xStep,(yCircle+yMid)/xStep]);
-          if ( dxy < dCircle ) {
-            ctx.fillStyle = 'rgb(255,255,255,0.5)';//'#FFFFFF';
-            ctx.fillRect(xOffset + x, yOffset + y, xStep+scale, yStep+scale);
-          }
-          ctx.fillStyle = 'rgb(255,0,0,'+((y*yMax+x)/cMax)+')';
-          ctx.fillRect(xOffset + xMid + xCircle, yOffset + yMid + yCircle, xStep+scale, yStep+scale);
+    const yMax    = yOffset + crop.height * scale;
+
+    ctxOffscreen.fillStyle = 'rgb(0,0,0,0.5)';
+    ctxOffscreen.fillRect(crop.x * scale, crop.y * scale, Math.floor((crop.width + 1) / 5) * xyStep, Math.floor((crop.height + 1) / 5) * xyStep);
+
+    for (let y = yOffset, yi = 0; y < yMax; y += xyStep, ++yi) {
+      for (let x = xOffset, xi = 0; x < xMax; x += xyStep, ++xi) {
+        if (0 == maskData[(xi + yi * canvasMask.width) * 4 + 0] && Math.random() < 0.3) {
+          ctxOffscreen.fillStyle = '#FFFFFF';
+          ctxOffscreen.fillRect(x, y, xyStep + scale, xyStep + scale);
         }
       }
     }
-*/
+
+    ctx.drawImage(canvasOffscreen, 0, 0);
+
+  }, [canvasRef, canvasOffscreenRef, canvasMaskRef, crop]);
+
+  // 範囲確定
+  const handleAreaLock = useCallback(() => {
+    const canvas = canvasRef.current;
     canvas.toBlob((imageBlob) => {
       onNextStep(imageBlob);
     }, 'image/jpeg', 0.9);
-  }, [crop]);
+  }, [image, crop]);
     
   return (
     <>
@@ -91,9 +96,11 @@ export default function ({ onNextStep, image }) {
           }
           crop={crop}
           onChange={newCrop => setCrop(newCrop)}
+          onComplete={() => handleChangeArea()}
         />
-        <canvas ref={canvasMaskRef} style={{ display: 'none' }} />
       </Card.Text>
+      <canvas ref={canvasOffscreenRef} style={{ width: '100%', display: 'none' }} />
+      <canvas ref={canvasMaskRef} style={{ display: 'none' }} />
     </>
   );
 }
