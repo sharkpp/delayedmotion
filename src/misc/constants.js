e085b432d5a1c065494bb8fbad3a14546869ed88
sharkpp <touroku@sharkpp.net>
2019-10-02 01:10:43 +0900

    追加 画像URLでの読み込み

--- src/misc/constants.js
+++ src/misc/constants.js
@@ -1,5 +1,11 @@
 "use struct";
 
+// 画像の元の場所
+export const ImageSourceType = {
+  FromLocal: 0,
+  FromUrl:   1,
+};
+
 // 処理のステップの状態
 export const Step = {
   UploadImage:   0,
--- src/views/UploadImage.js
+++ src/views/UploadImage.js
@@ -1,11 +1,22 @@
 "use struct";
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
+import { Tabs, Tab, Form, FormControl, InputGroup, Button, Spinner } from 'react-bootstrap';
 
 import {useDropzone} from 'react-dropzone'
 import 'react-dropzone/examples/theme.css';
 
+import { ImageSourceType } from '../misc/constants';
+
+import './UploadImage.css';
+
 export default function ({ onNextStep }) {
+
+  const [ imageSrcType, setImageSrcType ] = useState(ImageSourceType.FromLocal);
+  const [ imageUrl, setImageUrl ] = useState('');
+  const [ invalidImageUrl, setInvalidImageUrl ] = useState(false);
+  const [ loadingImageUrl, setLoadingImageUrl ] = useState(false);
 
   const handleDrop = (files) => {
     if (files[0]) {
@@ -36,16 +47,71 @@ export default function ({ onNextStep }) {
     </li>
   ));
 
+  const handleSelectImageUrl = useCallback((e) => {
+    setInvalidImageUrl(false);
+    setImageUrl(e.target.value);
+  });
+  const handleSelectedImageUrl = useCallback(() => {
+    setLoadingImageUrl(true);
+    setInvalidImageUrl(false);
+    const image = new Image();
+    image.crossOrigin = "anonymous";
+    image.src = 'https://api-gw98.herokuapp.com/' + imageUrl;
+    image.onload = (err) => {
+      setLoadingImageUrl(false);
+      onNextStep(image);
+    }
+    image.onerror = (err) => {
+      setLoadingImageUrl(false);
+      setInvalidImageUrl(true);
+    }
+  });
+
   return (
     <>
-      <div {...getRootProps({className: 'dropzone'})}>
-        <input {...getInputProps()} />
-        <p>画像をここにドロップするか、クリックして選択してください</p>
-        <em>jpeg もしくは png のみ指定可能)</em>
-      </div>
-      <ol>
-        {rejectedFilesItems}
-      </ol>
+      <Tabs activeKey={imageSrcType} onSelect={k => setImageSrcType(k)}>
+        <Tab eventKey={ImageSourceType.FromLocal} title="画像のアップロード">
+          <div {...getRootProps({className: 'dropzone'})}>
+            <input {...getInputProps()} />
+            <p>画像をここにドロップするか、クリックして選択してください</p>
+            <em>jpeg もしくは png のみ指定可能)</em>
+          </div>
+          <ol>
+            {rejectedFilesItems}
+          </ol>
+        </Tab>
+        <Tab eventKey={ImageSourceType.FromUrl} title="画像のURLを指定">
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="画像のURL"
+              value={imageUrl}
+              onChange={handleSelectImageUrl}
+              isInvalid={invalidImageUrl}
+              readOnly={loadingImageUrl}
+            />
+            <InputGroup.Append>
+              <Button 
+                variant="outline-primary"
+                onClick={handleSelectedImageUrl}
+                disabled={!imageUrl || loadingImageUrl}
+              >
+                {loadingImageUrl && <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                  className="btn-inner-spinner"
+                />}
+                画像を選択
+              </Button>
+            </InputGroup.Append>
+            <Form.Control.Feedback type="invalid">
+              指定したURLは画像として利用できませんでした
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Tab>
+      </Tabs>
     </>
   );
 }
